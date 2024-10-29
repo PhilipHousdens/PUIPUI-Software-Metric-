c0f406d4e89ca8aa160f61c0551a325cddf56f9d
Philip Housden <philip.houden.04@gmail.com>
2024-10-29 23:02:50 +0700

    adding getProductById

--- PUIPUI_POWER/src/controllers/puipuiDbController.js
+++ PUIPUI_POWER/src/controllers/puipuiDbController.js
@@ -1,6 +1,7 @@
 // puipuiDbController.js
 import { puipuiDB } from "../api/database/db.js"; // Ensure puipuiDB is an initialized collection
 
+// Get all puipui products
 async function getPuiPui() {
     try {
         const allPuiPui = await puipuiDB.find().toArray();
@@ -11,4 +12,25 @@ async function getPuiPui() {
     }
 }
 
-export { getPuiPui };
+async function getPuiPuiById(productId) {
+    try {
+        const product = await puipuiDB.findOne({ 
+            "products._id": parseInt(productId) // Use dot notation to search within the products array
+        },
+        {
+            projection: { "products.$": 1 } // Use projection to retrieve only the matching product
+        });
+
+        if (product && product.products.length > 0) {
+            return product.products[0]; // Return the matched product
+        } else {
+            return { error: "Product not found" };
+        }
+    } catch (error) {
+        console.error(`Error fetching PuiPui product with ID ${productId}:`, error);
+        return { error: error.message };
+    }
+}
+
+export { getPuiPui, getPuiPuiById };
+
