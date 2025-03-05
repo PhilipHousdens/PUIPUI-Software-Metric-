b0d7fcefa5c32d8aac76a24896216d237e8d8c1a
Bambam <bambam.pataranan@gmail.com>
2025-03-05 21:11:35 +0700

    update name constant in db.js

--- PUIPUI_POWER/src/api/database/db.js
+++ PUIPUI_POWER/src/api/database/db.js
@@ -8,13 +8,13 @@ const client = new MongoClient(MONGODB_URI);
 
 try {
     await client.connect();
-    await client.db("puipuiDB").command({ ping: 1 });
+    await client.db("productDB").command({ ping: 1 });
     console.log("Connected to MongoDB");
 } catch (error) {
     console.error("Error connecting to MongoDB: ", error);
 }
 
 const db = client.db(DB_NAME);
-const puipuiDB = db.collection("product_detail");
+const productDB = db.collection("product_detail");
 
-export { db, puipuiDB };
+export { db, productDB };
