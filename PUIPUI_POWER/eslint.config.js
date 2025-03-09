58ba066f9a914162d618eac7631849a1ac02fd4f
PhilipHousdens <philip.houden.04@gmail.com>
2025-03-09 21:03:28 +0700

    fixed: Parsing error: Unexpected token

--- PUIPUI_POWER/eslint.config.js
+++ PUIPUI_POWER/eslint.config.js
@@ -1,19 +1,34 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,ts,tsx,vue}'], // Added TS support
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+  {
+    name: 'app/rules',
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      vue: pluginVue,
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...js.configs.recommended.rules, // Spread correctly
+      ...pluginVue.configs['flat/essential'].rules,
+      ...ts.configs.recommended.rules,
+    },
+  },
   skipFormatting,
-]
+];
